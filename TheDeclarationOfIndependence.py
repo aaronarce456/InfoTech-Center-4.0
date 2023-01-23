8cdee295b9b8567fc80c9dfdd8ead12edb2bd924
Aaron Arce <aaronarce456@gmail.com>
2023-01-23 13:49:49 -0500

    Update Welcome Message

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -2,3 +2,10 @@
 # Date: 1.20.2023
 # Program: Infotech Center Upgrades
 
+"""
+Our Welcome Screen will start our Program letting
+drivers know that the InfoTech Center 4.0 OS is loading
+"""
+
+print("\n\nWelcome - InfoTechCenter 4.0")
+print("\nInfoTechCenter 4.0 OS is Loading")

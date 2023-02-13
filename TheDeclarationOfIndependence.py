1ba41736de6648f8348b28fc2c49450c5760dd0e
Aaron Arce <aaronarce456@gmail.com>
2023-02-13 13:56:22 -0500

    Added more elif and else to complete our list

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -7,15 +7,34 @@ import random
 
 # Create a weather conditions in a list and choose it randomly
 def weather():
-    weatherForecast = ["Snowing","Blizzard","Raining","Foggy","Windy","Icy","Sunshine"]
+    weatherForecast = ["Snowing","Blizzard","Rain","Foggy","Windy","Icy","Sunshine"]
     weatherCondition = random.choice(weatherForecast)
     return weatherCondition
 
 # Variable to call weather() once in our VRS()
 weatherAlert = weather()
 
-print(weatherAlert)
+
 
 # VRS() to respond to the weather conditions
-def vrs():
-    print("Howdy")
+def vRS():
+    if weatherAlert == "Snowing":
+        print("\nNWS has changed your Alarm by 15 minutes because of the weather forecast of",weatherAlert)
+        print("Your VRS has been engaged only allowing your vehicle to go 45 MPH.")
+    elif weatherAlert == "Blizzard":
+        print("\nNWS has changed your Alarm by 30 minutes because of the weather forecast of",weatherAlert)
+        print("Your VRS has been engaged only allowing your vehicle to go 35 MPH.")
+    elif weatherAlert == "Rain":
+        print("\nNWS is calling for,",weatherAlert,"please drive extra carefully.")
+    elif weatherAlert == "Foggy":
+        print("\nNWS is calling for",weatherAlert,"conditions, please drive extra careful.")
+    elif weatherAlert == "Windy":
+        print("\nNWS is calling for",weatherAlert,"conditions, please drive extra careful.")
+    elif weatherAlert == "Icy":
+        print("\nNWS has changed your Alarm by 60 minutes because of the weather forecast of",weatherAlert)
+        print("Your VRS has been engaged only allowing your vehicle to go 25 MPH.")
+    else:
+        print("\nNWS is calling for",weatherAlert,"drive safely and have a wonderful day.")
+
+
+vRS()

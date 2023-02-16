e203899407bc8f0151fe99f1778f168f57f12c3d
Aaron Arce <aaronarce456@gmail.com>
2023-02-16 13:13:27 -0500

    Made a contact list function

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,4 +1,21 @@
 # Programmer: Aaron Arce
-# Date: 1.20.2023
+# Date: 2.16.2023
 # Program: Infotech Center Upgrades
 
+"""
+We will make a function that shows contacts
+- Create a list of contacts
+-Print all contacts
+"""
+
+# Libraries Here
+import random
+
+
+
+def contacts():
+    contactList = ["Mom","Dad","Brother","Sister","Grandma","Grandpa","Aunt","Uncle"]
+    currentContact = random.choice(contactList)
+    return currentContact
+
+print("Alert!", contacts(),"is calling.")
